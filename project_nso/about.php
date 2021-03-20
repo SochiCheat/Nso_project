a8b6703e91c493998cec08f27f896f0d149ae07a
SochiCheatwep2020 <54494350+SochiCheatwep2020@users.noreply.github.com>
2021-03-20 09:19:03 +0700

    Add files via upload

---
+++ project_nso/about.php
@@ -0,0 +1,85 @@
+<?php include('./header.php') ?>
+
+<!-- About -->
+<div class="container">
+    <link rel="stylesheet" href="https://www.netsolution.online/images/icon1.png"
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+    <body>
+        <div id="container">
+            <div id="heading">
+                <h1>OUR SERVICE</h1>
+            </div>
+            <div class="item">
+                <div class="front"> <img id="team1" class="team"
+                        src="https://www.supercuts.com/content/dam/supercuts/www/models/2017-models/1x1/heroes/supercuts-spencer-hero-hairstyles-classic-fade-1x1.png/_jcr_content/renditions/cq5dam.web.320.320.medium.png">
+                </div>
+                <div class="back">
+                    <p class="title">Tom Crow</p>
+                    <p class="job">Interface Designer</p> <a href="#"><i
+                            class="fab fa-facebook-square social fa-2x"></i></a> <a href="#"><i
+                            class="fab fa-linkedin social fa-2x"></i></a> <a href="#"><i
+                            class="fab fa-twitter-square social fa-2x"></i></a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="front"> <img id="team2" class="team"
+                        src="https://www.eikonphoto.com/wp-content/uploads/2017/03/male-headshot-tie-color-e1515795186596.jpg">
+                </div>
+                <div class="back">
+                    <p class="title">Tom Crow</p>
+                    <p class="job">Interface Designer</p> <a href="#"><i
+                            class="fab fa-facebook-square social fa-2x"></i></a> <a href="#"><i
+                            class="fab fa-linkedin social fa-2x"></i></a> <a href="#"><i
+                            class="fab fa-twitter-square social fa-2x"></i></a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="front"> <img id="team3" class="team"
+                        src="https://www.ellasophiephoto.com/IMAGES/portraits/Portrait-Photographer-EllaSophie-OaklandStudio.jpg">
+                </div>
+                <div class="back">
+                    <p class="title">Tom Crow</p>
+                    <p class="job">Interface Designer</p> <a href="#"><i
+                            class="fab fa-facebook-square social fa-2x"></i></a> <a href="#"><i
+                            class="fab fa-linkedin social fa-2x"></i></a> <a href="#"><i
+                            class="fab fa-twitter-square social fa-2x"></i></a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="front"> <img id="team4" class="team"
+                        src="https://static1.squarespace.com/static/51047986e4b0f96ff4ffc8e0/t/58efb3ba5016e1685c551ae7/1492104152438/Mens-Accessories-Tie-Bow-pocket-square">
+                </div>
+                <div class="back">
+                    <p class="title">Tom Crow</p>
+                    <p class="job">Interface Designer</p> <a href="#"><i
+                            class="fab fa-facebook-square social fa-2x"></i></a> <a href="#"><i
+                            class="fab fa-linkedin social fa-2x"></i></a> <a href="#"><i
+                            class="fab fa-twitter-square social fa-2x"></i></a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="front"> <img id="team5" class="team"
+                        src="https://i.pinimg.com/originals/98/c8/0d/98c80de7cee9782322fdca55409bf6bc.jpg"> </div>
+                <div class="back">
+                    <p class="title">Tom Crow</p>
+                    <p class="job">Interface Designer</p> <a href="#"><i
+                            class="fab fa-facebook-square social fa-2x"></i></a> <a href="#"><i
+                            class="fab fa-linkedin social fa-2x"></i></a> <a href="#"><i
+                            class="fab fa-twitter-square social fa-2x"></i></a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="front"> <img id="team6" class="team"
+                        src="https://i.pinimg.com/originals/85/1e/7a/851e7a776927d4302e637ae511a3e372.jpg"> </div>
+                <div class="back">
+                    <p class="title">Tom Crow</p>
+                    <p class="job">Interface Designer</p> <a href="#"><i
+                            class="fab fa-facebook-square social fa-2x"></i></a> <a href="#"><i
+                            class="fab fa-linkedin social fa-2x"></i></a> <a href="#"><i
+                            class="fab fa-twitter-square social fa-2x"></i></a>
+                </div>
+            </div>
+        </div>
+    </body>
+</div>
